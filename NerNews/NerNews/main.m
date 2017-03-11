03184b97f5f6740e9bc98e0b6d9a5ba1f78fa41e
apple <276286811@qq.com>
2017-03-11 18:41:16 +0800

    项目准备

---
+++ NerNews/NerNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NerNews
+//
+//  Created by wangchen on 17/3/10.
+//  Copyright © 2017年 王晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ NerNews/Pods/Target Support Files/Pods-NerNews/Pods-NerNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NerNews : NSObject
+@end
+@implementation PodsDummy_Pods_NerNews
+@end
